fb052d2d2e4ea2488cb428d6b527c1b88b397b28
SilverFlin <toledorusso@outlook.com>
2022-05-18 13:44:01 -0700

    "Test commit"

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const DUMMY_EXPENESES = [
   { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: "e3",
-    title: "Car Insurance",
+    title: "Car Isurance",
     amount: 294.67,
     date: new Date(2021, 2, 28),
   },
